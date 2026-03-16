c0df6c8f2d4c2221ca5aa99d869eb0b36e99e1ca
YodoKKJ <beduschibrunocontato@gmail.com>
2026-03-16 20:21:12 -0300

    sim

---
+++ START.sql
@@ -0,0 +1,37 @@
+---------
+START
+    Read First Grade
+    Read Second Grade
+
+    IF First Grade OR Second Grade is NULL
+        Finish
+    ELSE
+    Final == SUM First and Second
+    Avarage == Final divide by 2
+
+    IF Avarage >= 7
+    Print APROVADO
+
+    Else 
+    REPROVADO
+END     
+
+---------------------
+
+START 
+    ASK NUM1 to USER
+
+    temp2 == CHECK NUM1 is INT and NUM1 > -1
+
+    IF temp2 == FALSE
+        SHOW "invalid number"
+    ELSE
+        WHILE X < NUM1
+            TEMP == DIVIDE NUM1 by 2 
+            IF TEMP == INT number
+                SHOW TEMP
+            ELSE 
+                Do Nothing
+            X++
+        WHILE END
+END
