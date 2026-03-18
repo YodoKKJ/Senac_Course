0f765591420278b95093293bb1ab08da477623c3
YodoKKJ <beduschibrunocontato@gmail.com>
2026-03-17 21:43:01 -0300

    sim

---
+++ bah.js
@@ -0,0 +1,9 @@
+let nome = "Bruno" 
+let idade = 22
+let helloWorld = "Hello World"
+
+console.log("Seu nome é " + nome + " voce tem " + idade + " anos de idade")
+console.log(helloWorld)
+
+
+const PI = 3.14159
---
+++ desafio1.js
@@ -0,0 +1,8 @@
+let numero = 5
+let tabuada = 10
+
+for (let i = 0; i <= tabuada; i++){
+    console.log(numero + " X " + i + " = " + (numero*tabuada))
+}
+
+console.log(" PINROT DASfas<dAKHFJKLDHFVbhfgbç")
