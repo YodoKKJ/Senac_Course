4476a29f1bcf0e21aaaf6c9e810c89e84acb973a
YodoKKJ <beduschibrunocontato@gmail.com>
2026-03-31 19:40:06 -0300

    aaaaa]

---
+++ lacosDeRepeticao.js
@@ -0,0 +1,41 @@
+
+for (let i = 10; i > -1 ; i-- ){
+    console.log(i)
+}
+
+
+//////////////////////////
+console.log("====================")
+///////////////////////
+
+let tabuada 
+
+for (let j = 0 ; j < 11; j++){
+
+    let conta = tabuada * j
+    console.log(tabuada + " x " + j + " = " + conta)}
+
+//////////////////////////
+console.log("====================")
+///////////////////////
+
+let num = 5;
+let a = 1;
+
+while (a <= 10) {
+
+  console.log(num + " x " + a + " = " + (num * a));
+  a++;
+
+}
+
+//////////////////////////
+console.log("====================")
+///////////////////////
+
+
+let contar = 5;
+do {
+  console.log(contar);
+  contar--; }
+while (contar >= 1);
